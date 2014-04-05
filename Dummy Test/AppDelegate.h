741a162bc04c6377f4ee78f74d2754e8a3f0b6ac
Jack Higgins <jackhiggins@Jacks-Mac-Pro.local>
2014-04-04 23:59:26 -0400

    Initial Commit

---
+++ Dummy Test/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Dummy Test
+//
+//  Created by Jack Higgins on 4/4/14.
+//  Copyright (c) 2014 Higgins Software Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Dummy Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dummy Test
+//
+//  Created by Jack Higgins on 4/4/14.
+//  Copyright (c) 2014 Higgins Software Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
